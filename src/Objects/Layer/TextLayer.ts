039e8a30be68f0c8ebdf7cf919cb8e345c8d1693
Jack Footner <jackrfootner@gmail.com>
2024-02-13 11:33:24 +1030

    Correctly seperate paragraphs and lines with appropriate types

--- src/Objects/Layer/TextLayer.ts
+++ src/Objects/Layer/TextLayer.ts
@@ -12,10 +12,31 @@ export interface StyleReplace {
   style: TextStyle
 }
 
+export interface StyledString {
+  string: string
+  style: TextStyle
+  metrics?: TextMetrics
+}
+
 export interface LineMetrics {
   maxAscent: number
   maxDescent: number
   width: number
+}
+
+export interface ParagraphMetrics {
+  height: number
+  width: number
+}
+
+export interface Line {
+  text: StyledString[]
+  metrics: LineMetrics
+}
+
+export interface Paragraph {
+  metrics: ParagraphMetrics
+  lines: Line[]
 }
 
 export class TextLayer extends Layer {
@@ -53,37 +74,53 @@ export class TextLayer extends Layer {
     const styledText = TextLayer.replaceTextStyles(replacedText, this.style, this.styleReplace)
 
     // Seperate paragraphs and line breaks
-    // TODO - Work out how to do paragraphs
-    const styledTextLines: Array<Array<{ text: string, style: TextStyle }>> = new Array<Array<{ text: string, style: TextStyle }>>()
-    styledTextLines.push(new Array<{ text: string, style: TextStyle }>())
+    const paragraphs = new Array<Paragraph>({ metrics: { height: 0, width: 0 }, lines: new Array<Line>({ metrics: { maxAscent: 0, maxDescent: 0, width: 0 }, text: new Array<StyledString>() }) })
     styledText.forEach(styledTextItem => {
-      const lines = TextLayer.seperateLineBreaks(styledTextItem.text)
-      styledTextLines[styledTextLines.length - 1].push({ text: lines[0], style: styledTextItem.style })
-      for (let index = 1; index < lines.length; index++) {
-        const lineText = lines[index]
-        styledTextLines.push(new Array<{ text: string, style: TextStyle }>())
-        styledTextLines[styledTextLines.length - 1].push({ text: lineText, style: styledTextItem.style })
+
+      // Split the text on double new lines for paragraphs
+      const paragraphSplits = styledTextItem.string.split("\n\n")
+      for (let paragraphTextIndex = 0; paragraphTextIndex < paragraphSplits.length; paragraphTextIndex++) {
+        const paragraphText = paragraphSplits[paragraphTextIndex];
+
+        // If this is not the fist index create a new paragraph with an empty line
+        if (paragraphTextIndex > 0) {
+          paragraphs.push({ metrics: { height: 0, width: 0 }, lines: new Array<Line>({ metrics: { maxAscent: 0, maxDescent: 0, width: 0 }, text: new Array<StyledString>() }) })
+        }
+        const currentParagraph = paragraphs[paragraphs.length - 1]
+
+        // Split the text on new lines as new lines
+        const lineSplits = paragraphText.split("\n")
+        for (let lineTextIndex = 0; lineTextIndex < lineSplits.length; lineTextIndex++) {
+          const lineText = lineSplits[lineTextIndex];
+
+          // If this is not the first index create a new line
+          if (lineTextIndex > 0) {
+            currentParagraph.lines.push({ metrics: { maxAscent: 0, maxDescent: 0, width: 0 }, text: new Array<StyledString>() })
+          }
+          const currentLine = currentParagraph.lines[currentParagraph.lines.length - 1]
+          currentLine.text.push({string: lineText, style: styledTextItem.style})
+
+          // Calculate new line metrics
+          TextLayer.setContextTextStyle(context, styledTextItem.style)
+          styledTextItem.metrics = context.measureText(lineText)
+
+          currentLine.metrics.maxAscent = Math.max(currentLine.metrics.maxAscent, styledTextItem.metrics.fontBoundingBoxAscent)
+          currentLine.metrics.maxDescent = Math.max(currentLine.metrics.maxDescent, styledTextItem.metrics.fontBoundingBoxDescent)
+          currentLine.metrics.width += styledTextItem.metrics.width
+        }
+
+        // Calculate paragrpah metrics
+        currentParagraph.lines.forEach(line => {
+          currentParagraph.metrics.height += (line.metrics.maxAscent + line.metrics.maxDescent)
+          currentParagraph.metrics.width = Math.max(currentParagraph.metrics.width, line.metrics.width)
+          // TODO - Add height for line spacing and paragraph spacing
+        });
       }
-    })
+    });
 
-    // Perform Word Wrapping/Scaling and Get Line Metrics
     // TODO - Word Wrapping/Scaling
-    const styledTextLinesWithMetrics: Array<{ metrics: LineMetrics, text: Array<{ text: string, style: TextStyle, metrics?: TextMetrics }> }> = new Array<{ metrics: LineMetrics, text: Array<{ text: string, style: TextStyle, metrics?: TextMetrics }> }>()
-    styledTextLines.forEach(line => {
-      styledTextLinesWithMetrics.push({ metrics: { maxAscent: 0, maxDescent: 0, width: 0 }, text: line })
-    })
 
-    let totalHeight = 0
-    styledTextLinesWithMetrics.forEach(line => {
-      line.text.forEach(styledText => {
-        TextLayer.setContextTextStyle(context, styledText.style)
-        styledText.metrics = context.measureText(styledText.text)
-        line.metrics.maxAscent = line.metrics.maxAscent > styledText.metrics.fontBoundingBoxAscent ? line.metrics.maxAscent : styledText.metrics.fontBoundingBoxAscent
-        line.metrics.maxDescent = line.metrics.maxDescent > styledText.metrics.fontBoundingBoxDescent ? line.metrics.maxDescent : styledText.metrics.fontBoundingBoxDescent
-        line.metrics.width += styledText.metrics.width
-      })
-      totalHeight += line.metrics.maxAscent + line.metrics.maxDescent
-    })
+    const totalHeight = paragraphs.reduce((p, c) => p + c.metrics.height, 0)
 
     // Draw text to canvas
     // Get start point vertically
@@ -100,37 +137,38 @@ export class TextLayer extends Layer {
         y = this.size.height - totalHeight
         break
     }
-    // for each line
-    for (let index = 0; index < styledTextLinesWithMetrics.length; index++) {
-      const line = styledTextLinesWithMetrics[index]
 
-      // Update y baseline
-      y += line.metrics.maxAscent
+    // for each paragraph
+    paragraphs.forEach(paragraph => {
+      paragraph.lines.forEach(line => {
+        // Update y baseline
+        y += line.metrics.maxAscent
 
-      // get start point horizontally
-      switch (this.align.horizontal) {
-        case 'Left':
-          x = 0
-          break
-        case 'Center':
-          x = (this.size.width / 2) - (line.metrics.width / 2)
-          break
-        case 'Right':
-          x = this.size.width - line.metrics.width
-          break
-      }
+        // get start point horizontally
+        switch (this.align.horizontal) {
+          case 'Left':
+            x = 0
+            break
+          case 'Center':
+            x = (this.size.width / 2) - (line.metrics.width / 2)
+            break
+          case 'Right':
+            x = this.size.width - line.metrics.width
+            break
+        }
 
-      // for each styled text
-      line.text.forEach(styledTextItem => {
-        // set context style, draw text, update horizontal position
-        TextLayer.setContextTextStyle(context, styledTextItem.style)
-        context.fillText(styledTextItem.text, x, y)
-        if (styledTextItem.metrics === undefined) throw new Error('Styled Text Item with no metrics')
-        x += styledTextItem.metrics.width
-      })
-      // update veritcal position
-      y += line.metrics.maxDescent
-    }
+        // for each styled text
+        line.text.forEach(styledTextItem => {
+          // set context style, draw text, update horizontal position
+          TextLayer.setContextTextStyle(context, styledTextItem.style)
+          context.fillText(styledTextItem.string, x, y)
+          if (styledTextItem.metrics === undefined) throw new Error('Styled Text Item with no metrics')
+          x += styledTextItem.metrics.width
+        })
+        // update veritcal position
+        y += line.metrics.maxDescent
+      });
+    });
 
     return canvas
   }
@@ -147,17 +185,17 @@ export class TextLayer extends Layer {
     return output
   }
 
-  getReplacedStyles = (text: string): Array<{ text: string, style: TextStyle }> => {
+  getReplacedStyles = (text: string): StyledString[] => {
     return TextLayer.replaceTextStyles(this.text, this.style, this.styleReplace)
   }
 
-  static replaceTextStyles = (text: string, originalStyle: TextStyle, replacementStyles: StyleReplace[]): Array<{ text: string, style: TextStyle }> => {
-    const styledText: Array<{ text: string, style: TextStyle }> = [{ text, style: originalStyle }]
+  static replaceTextStyles = (text: string, originalStyle: TextStyle, replacementStyles: StyleReplace[]): StyledString[] => {
+    const styledText: StyledString[] = [{ string: text, style: originalStyle }]
     for (let index = 0; index < replacementStyles.length; index++) {
       const styleReplacer = replacementStyles[index]
       for (let textIndex = 0; textIndex < styledText.length; textIndex++) {
         const styledTextItem = styledText[textIndex]
-        const startSplitText = styledTextItem.text.split(styleReplacer.startSymbol)
+        const startSplitText = styledTextItem.string.split(styleReplacer.startSymbol)
         let endSplitText, preText, contentText, postText
         if (startSplitText.length > 1) {
           endSplitText = startSplitText.slice(1).join(styleReplacer.startSymbol).split(styleReplacer.endSymbol)
@@ -166,18 +204,14 @@ export class TextLayer extends Layer {
             contentText = endSplitText[0]
             postText = endSplitText.splice(1).join(styleReplacer.endSymbol)
 
-            styledText[textIndex] = { text: preText, style: styledTextItem.style }
-            styledText[textIndex + 1] = { text: contentText, style: styleReplacer.style }
-            styledText[textIndex + 2] = { text: postText, style: styledTextItem.style }
+            styledText[textIndex] = { string: preText, style: styledTextItem.style }
+            styledText[textIndex + 1] = { string: contentText, style: styleReplacer.style }
+            styledText[textIndex + 2] = { string: postText, style: styledTextItem.style }
           }
         }
       }
     }
     return styledText
-  }
-
-  static seperateLineBreaks = (text: string): string[] => {
-    return text.split('\n')
   }
 
   static setContextTextStyle = (context: CanvasRenderingContext2D, style: TextStyle): void => {
