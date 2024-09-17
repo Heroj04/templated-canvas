f76758ecd90cc0b898590c324812805d41672bed
Jack Footner <jackrfootner@gmail.com>
2024-09-17 16:42:45 +0930

    Switch to using emHeight instead of ActualBoundingBox for text height

--- src/Objects/Layer/TextLayer.ts
+++ src/Objects/Layer/TextLayer.ts
@@ -105,8 +105,8 @@ export class TextLayer extends Layer {
           styledLineText.metrics = context.measureText(lineText)
 
           // TODO - Change to fontBoundingBox (No Support in node-canvas see https://github.com/Automattic/node-canvas/issues/1940)
-          currentLine.metrics.maxAscent = Math.max(currentLine.metrics.maxAscent, styledLineText.metrics.actualBoundingBoxAscent)
-          currentLine.metrics.maxDescent = Math.max(currentLine.metrics.maxDescent, styledLineText.metrics.actualBoundingBoxDescent)
+          currentLine.metrics.maxAscent = Math.max(currentLine.metrics.maxAscent, styledLineText.metrics.emHeightAscent)
+          currentLine.metrics.maxDescent = Math.max(currentLine.metrics.maxDescent, styledLineText.metrics.emHeightDescent)
           currentLine.metrics.width += styledLineText.metrics.width
         }
 
