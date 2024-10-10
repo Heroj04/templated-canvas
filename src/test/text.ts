08b3d7693e60b08d7a9302461f7afddf85e22a2f
Jack Footner <jackrfootner@gmail.com>
2024-10-10 15:59:07 +1030

    Add more to text test

--- src/test/text.ts
+++ src/test/text.ts
@@ -7,7 +7,8 @@ module.exports = {
 
     const layers = [
       new FillLayer('fill', 'Background', { x: 0, y: 0 }, { vertical: 'Top', horizontal: 'Left' }, { width: 100, height: 100 }, ['source-over'], 'white'),
-      new TextLayer('text', 'Test1', { x: 0, y: 0 }, { vertical: 'Top', horizontal: 'Left' }, { width: 100, height: 100 }, ['source-over'], 'Testing', { fillStyle: 'black', font: '10px' }, { vertical: 'Middle', horizontal: 'Center' }, false, false, new Map(), [])
+      new TextLayer('text', 'Test1', { x: 0, y: 0 }, { vertical: 'Top', horizontal: 'Left' }, { width: 100, height: 100 }, ['source-over'], 'Testing', { fillStyle: 'black', font: '10px' }, { vertical: 'Middle', horizontal: 'Center' }, false, false, new Map(), []),
+      new TextLayer('text', 'Test1', { x: 0, y: 0 }, { vertical: 'Top', horizontal: 'Left' }, { width: 100, height: 100 }, ['source-over'], 'abcdefghijklmnopqrstuvwxyz\nABCDEFGHIJKLMNOPQRSTUVWXYZ', { fillStyle: 'black', font: '10px' }, { vertical: 'Top', horizontal: 'Left' }, false, false, new Map(), [])
     ]
 
     const template = new Template('Test Text Template', 'Mr Hero', new URL('https://example.com'), { width: 100, height: 100 }, 300, [], layers)
